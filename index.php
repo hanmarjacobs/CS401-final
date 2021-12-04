430b53a9482e53a7586aff0546997ee15f3b5147
BoiseState Student <student@u.boisestate.edu>
2021-12-04 14:40:57 -0700

    PAGES - adding nav and edit/new post pages

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+<nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="newpost.php">New Post</a></li>
+    <li><a href="editpost.php">Edit Post</a></li>
+  </ul>
+</nav>
 
 <head>
   <link rel="stylesheet" href="css/style.css">
---
+++ newpost.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="newpost.php">New Post</a></li>
+    <li><a href="editpost.php">Edit Post</a></li>
+  </ul>
+</nav>
+</html>
